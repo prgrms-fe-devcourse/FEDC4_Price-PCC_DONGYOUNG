f8aa30c87bc914df92fc53e3a888d0027e2bfb28
Lim_JiSeon <dpf226@naver.com>
2023-09-08 18:47:57 +0900

    :tada: app path 정의

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,7 +1,13 @@
 const APP_PATH = {
   HOME: '/',
-  LOGIN: '/login',
-  REGISTER: '/register',
+  SIGNIN: '/signin',
+  SIGNUP: '/signup',
+  POST_DETAIL: '/post/[postid]',
+  POST_NEW: '/post/new',
+  SEARCH: '/search/[keyword]',
+  EDIT_PROFILE: '/user/edit/profile',
+  EDIT_PASSWORD: '/user/edit/password',
+  MYPAGE: '/user/mypage',
 } as const
 
 export default APP_PATH
