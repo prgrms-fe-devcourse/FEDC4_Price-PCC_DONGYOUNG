0f2ed3a1e931fe795faf20f43109409811e5e6d8
이민희 <mhl98@dgu.ac.kr>
2023-09-09 20:53:24 +0900

    ✨ 로컬스토리지 다크모드 여부 저장 (스토리지 hook 추가)

--- src/components/atoms/DarkModeButton/index.tsx
+++ src/components/atoms/DarkModeButton/index.tsx
@@ -1,30 +1,33 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import Assets from '@/config/assets'
+import useStorage from '@/hooks/useStorage'
 
 export default function DarkModeButton() {
-  const [mode, setMode] = useState(
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light',
-  )
+  const [darkMode, setDarkMode] = useStorage<boolean>({
+    storageType: 'local',
+    key: 'pcc-darkmode',
+    initialValue: window.matchMedia('(prefers-color-scheme: dark)').matches,
+  })
 
-  if (mode === 'dark') {
-    document.body.classList.add('pcc-theme--dark')
-    document.body.classList.remove('pcc-theme--light')
-  }
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('pcc-theme--dark')
+      document.body.classList.remove('pcc-theme--light')
+    }
+  }, [darkMode])
 
   const handleDarkmodeClick = () => {
     document.body.classList.toggle('pcc-theme--light')
     document.body.classList.toggle('pcc-theme--dark')
-    setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'))
+    setDarkMode(!darkMode)
   }
 
   return (
     <button onClick={handleDarkmodeClick}>
-      {mode === 'dark' ? (
+      {darkMode ? (
         <Image
           src={Assets.LIGHTMODE_SVG_PATH}
           width={30}
