46a60c486a4932e6dea9896aef262d9e4f68c7c4
AUTUMN <krshlee22@naver.com>
2023-09-10 12:52:21 +0900

    :tada: 로그인, 회원가입 공통 버튼 컴포넌트 추가

--- src/components/molcules/LoginButton/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function LoginButton() {
-  return <div>LoginButton</div>
-}
---
+++ src/components/molcules/SignButton/index.tsx
@@ -0,0 +1,9 @@
+import './index.scss'
+
+interface SignButtonProps {
+  text?: string
+}
+
+export default function SignButton({ text }: SignButtonProps) {
+  return <button className="sign-button-container">{text}</button>
+}
