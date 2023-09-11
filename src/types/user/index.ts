f74a5cf6dc22cd07b2bae45566515303cc945a68
Kim0426 <706shin1728@naver.com>
2023-09-11 12:34:30 +0900

    🎉  공통의 타입구문 types폴더로 분리

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,6 +1,32 @@
+import Notification from '../Notification'
+import Like from '../like'
+import Message from '../message'
+import Post from '../post'
+
 interface User {
-  name: string
-  age: number
+  coverImage: string
+  image: string
+  role: string
+  isOnline: boolean
+  posts: Post[]
+  likes: Like[]
+  comments: string[]
+  followers: string[]
+  following: Following[]
+  notifications: Notification[]
+  messages: Message[]
+  _id: string
+  fullName: string
+  email: string
+  createdAt: string
+  updatedAt: string
 }
 
+interface Following {
+  _id: string
+  user: string
+  follower: string
+  createdAt: string
+  updatedAt: string
+}
 export default User
