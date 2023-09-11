acd8cbb009e020433b2c064046b2cc1e8854831c
Kim0426 <706shin1728@naver.com>
2023-09-12 00:46:30 +0900

    🎉  초기 게시글 상세 페이지로 들어갔을떄 초기 데이터 로드

--- src/app/(root)/(routes)/post/[postId]/page.tsx
+++ src/app/(root)/(routes)/post/[postId]/page.tsx
@@ -1,3 +1,23 @@
-export default function Post() {
+import { headers } from 'next/headers'
+import { Environment } from '@/config/environments'
+
+const getInitPost = async () => {
+  const postHeaders = headers()
+  const getPathName = postHeaders.get('x-invoke-path')?.replaceAll('/post/', '')
+
+  const getInitPostRes = await fetch(
+    `${Environment.baseUrl()}/posts/${getPathName}`,
+    {
+      cache: 'no-cache',
+    },
+  )
+
+  const getPostDetail = await getInitPostRes.json()
+  return await getPostDetail
+}
+
+export default async function Post() {
+  const res = await getInitPost()
+  console.log(res)
   return <div>글 상세 페이지</div>
 }
