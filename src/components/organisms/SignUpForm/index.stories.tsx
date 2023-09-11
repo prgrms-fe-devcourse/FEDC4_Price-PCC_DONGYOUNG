23b7bda8d5f8983b6b5fa5554ccec154cff6c1ca
AUTUMN <krshlee22@naver.com>
2023-09-11 16:52:17 +0900

    :tada: 회원가입 폼 컴포넌트 구현

---
+++ src/components/organisms/SignUpForm/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import SignUpForm from '.'
+
+const meta = {
+  title: 'Component/SignUpForm',
+  component: SignUpForm,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof SignUpForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SignUp: Story = {}
