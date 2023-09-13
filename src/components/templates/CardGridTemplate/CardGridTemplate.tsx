262cb968681e94237a7b7c48843ceb481eb326ef
이민희 <mhl98@dgu.ac.kr>
2023-09-13 22:19:18 +0900

    :tada: 그리드 템플릿 추가

---
+++ src/components/templates/CardGridTemplate/CardGridTemplate.tsx
@@ -0,0 +1,23 @@
+import { CardPostItem } from '@/components/organisms/CardPostItem'
+import { CardPostItemProps } from '@/components/organisms/CardPostItem/CardPostItem'
+import Post from '@/types/post'
+import './index.scss'
+
+type CardGridTemplateProps = {
+  postDatas: Post[]
+}
+export default function CardGridTemplate({ postDatas }: CardGridTemplateProps) {
+  return (
+    <div className="card-grid-container">
+      {postDatas.map(({ _id, image, author, title }: CardPostItemProps) => (
+        <CardPostItem
+          key={_id}
+          _id={_id}
+          image={image}
+          author={author}
+          title={title}
+        />
+      ))}
+    </div>
+  )
+}
