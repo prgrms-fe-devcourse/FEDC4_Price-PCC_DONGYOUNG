fd2f89d01580c84257b8e2082c3fca40698d1cb6
이민희 <mhl98@dgu.ac.kr>
2023-09-13 22:18:27 +0900

    :sparkles: 카드 컴포넌트 props받도록 수정, 스토리북 삭제..

--- src/components/organisms/CardPostItem/CardPostItem.tsx
+++ src/components/organisms/CardPostItem/CardPostItem.tsx
@@ -1,47 +1,47 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Avatar from '@/components/atoms/Avatar'
 import { Card } from '@/components/atoms/Card'
 import { Text } from '@/components/atoms/Text'
 import { LikeDislikeCount } from '@/components/molcules/LikeDislikeCount'
+import Post from '@/types/post'
 import './index.scss'
 
-const DUMMY = {
-  _id: 0,
-  image: 'https://picsum.photos/200/300',
-  author: '이민희',
-  title: {
-    title: '제목',
-    body: '글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?글 내용입니다. 포스트 api는 글 내용을 필드로 안 담고 있네요..?',
-  },
-}
-export default function CardPostItem() {
-  const {
-    _id,
-    image,
-    author,
-    title: { title, body },
-  } = DUMMY
+export type CardPostItemProps = Pick<Post, '_id' | 'image' | 'author' | 'title'>
+
+export default function CardPostItem({
+  _id,
+  image,
+  author,
+  title,
+}: CardPostItemProps) {
   return (
     <Card width={22} height={20.25}>
       <div className="content-container">
-        <Avatar text={author} size={1.25} src={image} />
-        <Text textStyle="body1-bold">{title}</Text>
+        <Link href={`/user/${author._id}`}>
+          <Avatar text={author.fullName} size={1.25} src={image} />
+        </Link>
+        <Link href={`/post/${_id}`}>
+          <Text textStyle="body1-bold">{JSON.parse(title).title}</Text>
+        </Link>
         {image ? (
           <div className="content-container__image-container">
             <Image src={image} alt="첨부 이미지" fill />
           </div>
         ) : (
-          <Text
-            textStyle="body2"
-            style={{
-              display: '-webkit-box',
-              WebkitLineClamp: 8,
-              WebkitBoxOrient: 'vertical',
-              overflow: 'hidden',
-            }}
-          >
-            {body}
-          </Text>
+          <Link href={`/post/${_id}`}>
+            <Text
+              textStyle="body2"
+              style={{
+                display: '-webkit-box',
+                WebkitLineClamp: 8,
+                WebkitBoxOrient: 'vertical',
+                overflow: 'hidden',
+              }}
+            >
+              {JSON.parse(title).body}
+            </Text>
+          </Link>
         )}
         <LikeDislikeCount like={230} dislike={170} />
       </div>
