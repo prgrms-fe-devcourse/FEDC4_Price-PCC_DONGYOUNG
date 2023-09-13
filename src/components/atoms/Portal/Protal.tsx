83dfe15261f3d8125ea58acc3b6310aeeef1c494
Kim0426 <706shin1728@naver.com>
2023-09-13 13:36:31 +0900

    🎉 모달을 위한 portal 컴포넌트 추가

---
+++ src/components/atoms/Portal/Protal.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+
+type PortalProps = {
+  children: ReactNode
+}
+
+export default function Portal({ children }: PortalProps) {
+  const node = document.getElementById('modal') as HTMLElement
+  return createPortal(children, node)
+}
