03317e16657646b4a51829accc93c827d09ca3dd
Kim0426 <706shin1728@naver.com>
2023-09-13 16:16:59 +0900

    ✨  모달 상태 관리하는 훅 추가

---
+++ src/components/atoms/Modal/useModal.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export default function useModal() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
+  return { isOpen, openModal, closeModal }
+}
