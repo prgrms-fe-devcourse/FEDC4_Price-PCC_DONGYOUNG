c025e85a94a6fc73e98c3107a4fad563f362f393
이민희 <mhl98@dgu.ac.kr>
2023-09-13 17:03:05 +0900

    :broom: likedislike 경로 문제로 삭제 후 새로 생성, 숫자에 text컴포넌트 적용

--- src/components/molcules/LikeDislikeCount/index.stories.ts
+++ src/components/molcules/LikeDislikeCount/index.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import LikeDislikeCount from './LikeDislikeCount'
+import { LikeDislikeCount } from '.'
 
 const meta = {
   title: 'Component/LikeDislikeCount',
