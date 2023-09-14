a3e568ecb90b97805fa391d21af858d4d02db565
이민희 <mhl98@dgu.ac.kr>
2023-09-15 04:46:05 +0900

    :sparkles: limit 상수처리

--- src/queries/channel/index.ts
+++ src/queries/channel/index.ts
@@ -1,13 +1,16 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { constants } from '@/config/constants'
 import { getAllPosts } from '@/services/channel'
 
 const useGetAllPosts = (channelId: string) => {
-  const LIMIT = 10
-
   return useInfiniteQuery({
     queryKey: ['getAllPostsInfiniteQuery'],
     queryFn: ({ pageParam = 0 }) =>
-      getAllPosts({ channelId, offset: pageParam, limit: LIMIT }),
+      getAllPosts({
+        channelId,
+        offset: pageParam,
+        limit: constants.INFINITE_LIMIT,
+      }),
     getNextPageParam: (lastPage, allPages) => allPages.flat().length,
   })
 }
