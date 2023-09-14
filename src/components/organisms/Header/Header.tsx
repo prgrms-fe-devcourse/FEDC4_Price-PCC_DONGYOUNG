d3dfdf2fa277cd1888eee52143b27f43cdadd4b1
Lim_JiSeon <dpf226@naver.com>
2023-09-15 01:32:03 +0900

    :tada: 헤더 검색바, 알림버튼, 다크모드 버튼 적용 완료

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,5 +1,14 @@
+'use client'
+
+import SearchBar from '@/components/atoms/SearchBar'
+import HeaderButtons from '@/components/molcules/HeaderButtons'
 import './index.scss'
 
 export default function Header() {
-  return <div className="header-container color-bg--bg-1"></div>
+  return (
+    <div className="header-container color-bg--bg-1">
+      <SearchBar />
+      <HeaderButtons />
+    </div>
+  )
 }
