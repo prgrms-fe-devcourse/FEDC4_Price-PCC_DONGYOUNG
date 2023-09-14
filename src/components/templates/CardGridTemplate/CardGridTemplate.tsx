8326d6f5fddcdfc3a72f47ddef6960757449cdb0
이민희 <mhl98@dgu.ac.kr>
2023-09-15 00:05:20 +0900

    :sparkles: &&연산자 옵셔널로 변경

--- src/components/templates/CardGridTemplate/CardGridTemplate.tsx
+++ src/components/templates/CardGridTemplate/CardGridTemplate.tsx
@@ -9,19 +9,18 @@ type CardGridTemplateProps = {
 export default function CardGridTemplate({ postDatas }: CardGridTemplateProps) {
   return (
     <div className="card-grid-container">
-      {postDatas &&
-        postDatas.map(
-          ({ _id, image, author, title, description }: CardPostItemProps) => (
-            <CardPostItem
-              key={_id}
-              _id={_id}
-              image={image}
-              author={author}
-              title={title}
-              description={description}
-            />
-          ),
-        )}
+      {postDatas?.map(
+        ({ _id, image, author, title, description }: CardPostItemProps) => (
+          <CardPostItem
+            key={_id}
+            _id={_id}
+            image={image}
+            author={author}
+            title={title}
+            description={description}
+          />
+        ),
+      )}
     </div>
   )
 }
