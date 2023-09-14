75dc40b3679931229cfebd23062537e6dd7d245c
이민희 <mhl98@dgu.ac.kr>
2023-09-15 04:41:04 +0900

    :tada: 무한스크롤 기능

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -1,11 +1,32 @@
 'use client'
 
+import { useEffect, useRef } from 'react'
 import CardGridTemplate from '@/components/templates/CardGridTemplate'
 import useGetAllPosts from '@/queries/channel'
 
 const CHANNEL_ID = process.env.NEXT_PUBLIC_CHANNEL_ID as string
 
 export default function Home() {
-  const { data } = useGetAllPosts(CHANNEL_ID)
-  return <CardGridTemplate postDatas={data?.posts} />
+  const { data, fetchNextPage, hasNextPage } = useGetAllPosts(CHANNEL_ID)
+
+  const observerElem = useRef(null)
+
+  useEffect(() => {
+    const element = observerElem.current
+    const option = { threshold: 0.5 }
+
+    const io = new IntersectionObserver((entries) => {
+      const [target] = entries
+      if (target.isIntersecting) {
+        fetchNextPage()
+      }
+    }, option)
+
+    if (hasNextPage && element) {
+      io.observe(element)
+      return () => io.unobserve(element)
+    }
+  }, [fetchNextPage, hasNextPage])
+
+  return <CardGridTemplate postDatas={data?.pages.flat()} ref={observerElem} />
 }
--- src/services/channel/index.ts
+++ src/services/channel/index.ts
@@ -1,15 +1,17 @@
 import { apiClient } from '@/lib/axios'
+import Post from '@/types/post'
 
 type GetAllPosts = {
   channelId: string
   offset: number
   limit: number
 }
+
 export const getAllPosts = async ({
   channelId,
   offset,
   limit,
-}: GetAllPosts) => {
+}: GetAllPosts): Promise<Post[]> => {
   try {
     const { data } = await apiClient.get(
       `api/channel/${channelId}?offset=${offset}&limit=${limit}`,
@@ -17,5 +19,6 @@ export const getAllPosts = async ({
     return data
   } catch (e) {
     if (e instanceof Error) throw new Error(e.message)
+    return []
   }
 }
