4885c6268da86a708a7226478346c77a6df8826f
Lim_JiSeon <dpf226@naver.com>
2023-09-15 06:51:51 +0900

    :tada: NavBar 스토리북 및 파일 구조 재편성

---
+++ src/components/organisms/NavBar/NavBar.tsx
@@ -0,0 +1,5 @@
+import './index.scss'
+
+export default function NavBar() {
+  return <div className="nav-container">NavBar</div>
+}
---
+++ src/components/organisms/NavBar/index.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import NavBar from '.'
+
+const meta = {
+  title: 'Component/NavBar',
+  component: NavBar,
+} satisfies Meta<typeof NavBar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
