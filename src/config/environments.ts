3bb7e60eb7c0141604d49a0bd231b957430f1949
oaoong <pizzachicken@kakao.com>
2023-09-16 11:29:01 +0900

    :bug: actions에 env 적용

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,9 +1,11 @@
+import { assertValue } from '@/utils/assertValue'
+
 export const Environment = {
-
-  nodeEnv: () => process.env.NODE_ENV!,
-  baseUrl: () => process.env.NEXT_PUBLIC_API_ADDRESS!,
-  internalApiAddress: () => process.env.NEXT_PUBLIC_INTERNAL!,
-  channelId: () => process.env.NEXT_PUBLIC_CHANNEL_ID!,
-
+  nodeEnv: () => process.env.NODE_ENV,
+  baseUrl: () => assertEnv('NEXT_PUBLIC_API_ADDRESS'),
+  channelId: () => assertEnv('NEXT_PUBLIC_CHANNEL_ID'),
 }
 
+function assertEnv(key: string) {
+  return assertValue<string>(process.env[key], `${key}가 설정되지 않았습니다.`)
+}
