a19ece514f72c2ba8bca0911c01031db9188abd7
Kim0426 <706shin1728@naver.com>
2023-09-18 22:44:24 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
