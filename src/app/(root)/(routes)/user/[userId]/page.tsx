2e92bd2aed5d9ec11602da31a8b698067cc7fce1
oaoong <pizzachicken@kakao.com>
2023-09-18 22:18:29 +0900

    :tada: 유저 포스트 불러오기

--- src/app/(root)/(routes)/user/[userId]/page.tsx
+++ src/app/(root)/(routes)/user/[userId]/page.tsx
@@ -1,5 +1,18 @@
-import UserDetailPageTemplate from '@/components/templates/UserDetailPageTemplate'
+'use client'
 
-export default function User() {
-  return <UserDetailPageTemplate />
+import UserDetailPageTemplate from '@/components/templates/UserDetailPageTemplate'
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
+import useGetUserPosts from '@/queries/posts'
+
+export default function User({
+  params: { userId },
+}: {
+  params: { userId: string }
+}) {
+  const { data, fetchNextPage, hasNextPage } = useGetUserPosts(userId)
+  const { observerElem } = useInfiniteScroll({ fetchNextPage, hasNextPage })
+
+  return (
+    <UserDetailPageTemplate userPosts={data?.pages.flat()} ref={observerElem} />
+  )
 }
--- src/components/templates/UserDetailPageTemplate/UserDetailPageTemplate.tsx
+++ src/components/templates/UserDetailPageTemplate/UserDetailPageTemplate.tsx
@@ -1,14 +1,23 @@
+import { ForwardedRef, forwardRef } from 'react'
 import UserDetailCard from '@/components/organisms/UserDetailCard'
+import type Post from '@/types/post'
 import CardGridTemplate from '../CardGridTemplate'
 import './index.scss'
 
-export default function UserDetailPageTemplate() {
+type UserDetailPageTemplateProps = {
+  userPosts: Post[] | undefined
+}
+
+export default forwardRef(function UserDetailPageTemplate(
+  { userPosts }: UserDetailPageTemplateProps,
+  ref: ForwardedRef<null>,
+) {
   return (
     <div className="user-detail">
       <UserDetailCard />
       <div className="grid">
-        <CardGridTemplate postDatas={[]} />
+        <CardGridTemplate postDatas={userPosts} ref={ref} />
       </div>
     </div>
   )
-}
+})
