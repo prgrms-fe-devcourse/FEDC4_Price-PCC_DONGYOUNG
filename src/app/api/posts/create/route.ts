100395b59a7268a074217a61523c112c45831bc0
Kim0426 <706shin1728@naver.com>
2023-09-18 22:47:32 +0900

    🎉 Promise.all 사용해서 api 호출

--- src/app/api/posts/create/route.ts
+++ src/app/api/posts/create/route.ts
@@ -2,19 +2,43 @@ import { NextResponse } from 'next/server'
 import { Environment } from '@/config/environments'
 import { useServerCookie } from '@/hooks/useServerCookie'
 import { apiServer } from '@/lib/axiosSever'
+import Post from '@/types/post'
 
 export async function POST(request: Request) {
-  const formData = await request.formData()
-  const channelId = Environment.channelId()
-  formData.append('channelId', channelId)
   const { token } = useServerCookie()
+  const { channelId, likeChannelId, dislikeChannelID } = Environment
+  const channelIds = [channelId(), likeChannelId(), dislikeChannelID()]
 
-  const { data } = await apiServer.post(`/posts/create`, formData, {
-    headers: {
-      Authorization: 'Bearer ' + token,
-      'Content-Type': 'multipart/form-data',
-    },
-  })
+  const createPost = async (
+    formData: FormData,
+    channelId: string,
+  ): Promise<Post> => {
+    if (formData.has('channelId')) {
+      formData.delete('channelId')
+    }
+    formData.append('channelId', channelId)
 
-  return NextResponse.json(data)
+    const { data } = await apiServer.post('/posts/create', formData, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    return data
+  }
+
+  const formData = await request.formData()
+
+  const apiCallPromises = []
+
+  for (const channelId of channelIds) {
+    apiCallPromises.push(createPost(formData, channelId))
+  }
+
+  console.log(apiCallPromises)
+
+  const results = await Promise.all(apiCallPromises)
+
+  return NextResponse.json(results[0])
 }
