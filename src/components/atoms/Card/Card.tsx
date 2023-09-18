770eaf15ee63c868a6191e3c4bc27931c810edb6
이민희 <mhl98@dgu.ac.kr>
2023-09-18 17:58:24 +0900

    :sparkles: card컴포넌트 width, height props추가

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -3,12 +3,30 @@ import './index.scss'
 
 export type CardProps = {
   children: ReactNode
+  width?: number
+  height?: number
   style?: CSSProperties
 }
 
-export default function Card({ children, ...props }: CardProps) {
+/**
+ * 카드 컴포넌트
+ * @param width 단위: rem
+ * @param height 단위: rem
+ * @returns children을 포함한 카드 컴포넌트
+ */
+export default function Card({
+  children,
+  width,
+  height,
+  style,
+  ...props
+}: CardProps) {
   return (
-    <div className="card-container" {...props}>
+    <div
+      className="card-container"
+      {...props}
+      style={{ width: `${width}rem`, height: `${height}rem`, ...style }}
+    >
       {children}
     </div>
   )
