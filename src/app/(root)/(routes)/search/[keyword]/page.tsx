554307afd5e191afcb20f67782ab5acbd1ee7947
Lim_JiSeon <dpf226@naver.com>
2023-09-19 15:55:40 +0900

    :tada: api 연결 중

--- src/app/(root)/(routes)/search/[keyword]/page.tsx
+++ src/app/(root)/(routes)/search/[keyword]/page.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
 export default function Search() {
+  const pathname = usePathname()
+  console.log(pathname.split('/')[2])
+
   return <div>검색 페이지</div>
 }
