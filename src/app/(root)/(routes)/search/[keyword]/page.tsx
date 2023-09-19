3234031685c70fa023697f6dbf1165c2c67181c1
Lim_JiSeon <dpf226@naver.com>
2023-09-19 16:40:40 +0900

    :tada: 데이터 가져오는 함수 생성

--- src/app/(root)/(routes)/search/[keyword]/page.tsx
+++ src/app/(root)/(routes)/search/[keyword]/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
+import { searchData } from '@/services/search'
 
 export default function Search() {
   const pathname = usePathname()
-  console.log(pathname.split('/')[2])
+  const keyword = pathname.split('/')[2]
+  console.log(searchData(keyword))
 
   return <div>검색 페이지</div>
 }
