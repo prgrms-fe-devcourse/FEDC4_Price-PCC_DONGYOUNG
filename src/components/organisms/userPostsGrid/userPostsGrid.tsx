b69e24f4f1a7fdf6e1bbd00e51e8e8269b017825
Kim0426 <706shin1728@naver.com>
2023-09-21 16:09:32 +0900

    🎉 초기 유저 디테일 페이지에서 현재 좋아요 채널의 게시글만 불러오게 설정

--- src/components/organisms/userPostsGrid/userPostsGrid.tsx
+++ src/components/organisms/userPostsGrid/userPostsGrid.tsx
@@ -3,13 +3,19 @@
 import CardGridTemplate from '@/components/templates/CardGridTemplate'
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
 import useGetUserPosts from '@/queries/posts'
+import Post from '@/types/post'
 import './index.scss'
 
 export default function UserPostsGrid({ userId }: { userId: string }) {
   const { data, fetchNextPage, hasNextPage } = useGetUserPosts(userId)
   const { observerElem } = useInfiniteScroll({ fetchNextPage, hasNextPage })
 
-  const posts = data?.pages.flat()
+  const uniquePosts = data?.pages.flat().reduce((unique, post) => {
+    if (!unique.some((uniquePost: Post) => uniquePost._id === post._id)) {
+      unique.push(post)
+    }
+    return unique
+  }, [])
 
-  return <CardGridTemplate postDatas={posts} ref={observerElem} />
+  return <CardGridTemplate postDatas={uniquePosts} ref={observerElem} />
 }
