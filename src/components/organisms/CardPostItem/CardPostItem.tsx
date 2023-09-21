f275b5ab48a3bfde7e408d0b520e4aedba8d6c05
이민희 <mhl98@dgu.ac.kr>
2023-09-21 12:29:40 +0900

    :tada: 유저 디테일 페이지에서 게시글 옵션 추가

--- src/components/organisms/CardPostItem/CardPostItem.tsx
+++ src/components/organisms/CardPostItem/CardPostItem.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Avatar from '@/components/atoms/Avatar'
 import { Card } from '@/components/atoms/Card'
 import { Text } from '@/components/atoms/Text'
 import { LikeDislikeCount } from '@/components/molcules/LikeDislikeCount'
+import { PostOptionDropdownList } from '@/components/molcules/ModalDropdownList'
+import Assets from '@/config/assets'
 import APP_PATH from '@/config/paths'
 import Post from '@/types/post'
 import './index.scss'
@@ -20,12 +23,21 @@ export default function CardPostItem({
   title,
   description,
 }: CardPostItemProps) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
   return (
     <Card>
       <div className="content-container">
-        <Link href={APP_PATH.userProfile(author._id)}>
-          <Avatar text={author.fullName} size={1.25} src={image} />
-        </Link>
+        <div className="content-container__header">
+          <Link href={APP_PATH.userProfile(author._id)}>
+            <Avatar text={author.fullName} size={1.25} src={image} />
+          </Link>
+          <Image
+            src={Assets.OptionsIcon}
+            alt="더보기 아이콘"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          />
+          <PostOptionDropdownList isOpen={isDropdownOpen} postId={_id} />
+        </div>
         <Link href={`/post/${_id}`}>
           <Text textStyle="body1-bold">{title}</Text>
         </Link>
