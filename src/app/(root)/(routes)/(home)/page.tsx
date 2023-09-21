ada3514f570207f2ef3980266f2e6db988605ba0
이민희 <mhl98@dgu.ac.kr>
2023-09-21 19:53:13 +0900

    :tada: 페이지에 붙임

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import CirclePlusButton from '@/components/atoms/CirclePlusButton'
 import CardGridTemplate from '@/components/templates/CardGridTemplate'
+import APP_PATH from '@/config/paths'
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
 import useGetAllPosts from '@/queries/channel'
 
@@ -8,6 +11,12 @@ export default function Home() {
   const { data, fetchNextPage, hasNextPage } = useGetAllPosts()
 
   const { observerElem } = useInfiniteScroll({ fetchNextPage, hasNextPage })
+  const router = useRouter()
 
-  return <CardGridTemplate postDatas={data?.pages.flat()} ref={observerElem} />
+  return (
+    <>
+      <CardGridTemplate postDatas={data?.pages.flat()} ref={observerElem} />
+      <CirclePlusButton onClick={() => router.push(APP_PATH.postNew())} />
+    </>
+  )
 }
