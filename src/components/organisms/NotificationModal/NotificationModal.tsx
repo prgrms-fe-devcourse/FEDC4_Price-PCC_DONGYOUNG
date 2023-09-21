05547e437ac68f9b7c430b61c19d466483edd2be
Lim_JiSeon <dpf226@naver.com>
2023-09-21 18:14:06 +0900

    :tada: 알림 모달창 구현

---
+++ src/components/organisms/NotificationModal/NotificationModal.tsx
@@ -0,0 +1,58 @@
+import Avatar from '@/components/atoms/Avatar'
+import { Card } from '@/components/atoms/Card'
+import { Text } from '@/components/atoms/Text'
+import Assets from '@/config/assets'
+import { NOTIFICATION_CONSTANT } from '@/constants/notification'
+import Comment from '@/types/comment'
+import Notification from '@/types/notification'
+import User from '@/types/user'
+import './index.scss'
+
+type PropsType = {
+  data: Notification[] | undefined
+}
+
+type NotificationProps = {
+  seen: boolean
+  _id: string
+  user: User
+  follow?: string
+  comment?: Comment
+}
+
+export default function NotificationModal({ data }: PropsType) {
+  return (
+    <Card>
+      <Text className="notification-title" textStyle="body1">
+        알림
+      </Text>
+      {data?.map(
+        ({ seen, _id, user, follow, comment }: NotificationProps) =>
+          seen && (
+            <Avatar
+              key={_id}
+              src={user.image || Assets.PCCImage}
+              size={3}
+              text={user.fullName}
+              subText={
+                follow
+                  ? NOTIFICATION_CONSTANT.FOLLOW
+                  : comment
+                  ? NOTIFICATION_CONSTANT.COMMENT
+                  : ''
+              }
+            />
+          ),
+      )}
+      <button className="seen-button" onClick={() => alert('모두 읽음')}>
+        <Text
+          className="seen-button-text"
+          textStyle="body2-bold"
+          color="primary-4"
+        >
+          모두 읽음
+        </Text>
+      </button>
+    </Card>
+  )
+}
