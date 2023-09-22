1daa2f5ff895f25d21f16bb18456d5d199e209a2
AUTUMN <krshlee22@naver.com>
2023-09-22 15:38:58 +0900

    :tada: 로그인 및 회원가입 에러 처리

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,6 +2,7 @@ import { AxiosError } from 'axios'
 import { useRouter } from 'next/navigation'
 import { notify } from '@/components/atoms/Toast'
 import APP_PATH from '@/config/paths'
+import { SIGN_CONSTANT } from '@/constants/sign'
 import { loginUser } from '@/services/auth'
 
 export interface LoginReqBody {
@@ -19,8 +20,8 @@ export const useLogin = () => {
       }
     } catch (error) {
       const { response } = error as unknown as AxiosError
-      if (response?.data) {
-        notify('error', '로그인에 실패했습니다.')
+      if ((response?.data as any).error === SIGN_CONSTANT.LOGINREJECT) {
+        notify('error', '아이디와 비밀번호를 다시 확인해 주세요')
       }
     }
     return null
