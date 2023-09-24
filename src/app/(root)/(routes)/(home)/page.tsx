a429f8a3d40ade4d2129fa2eca9cd27cd72327bc
oaoong <pizzachicken@kakao.com>
2023-09-25 03:23:24 +0900

    :sparkles: 포스트 리스트 첫 페이지 프리패치

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -1,29 +1,16 @@
-'use client'
-
 import React from 'react'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import CirclePlusButton from '@/components/atoms/CirclePlusButton'
-import CardGridTemplate from '@/components/templates/CardGridTemplate'
+import HydrateCardGrid from '@/components/templates/CardGridTemplate/HydrateCardGrid'
 import APP_PATH from '@/config/paths'
-import { useCurrentUser } from '@/hooks/useCurrentUser'
-import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
-import useGetAllPosts from '@/queries/channel'
 
 export default function Home() {
-  const { currentUser } = useCurrentUser()
-  const { data, fetchNextPage, hasNextPage } = useGetAllPosts(currentUser)
-
-  const { observerElem } = useInfiniteScroll({ fetchNextPage, hasNextPage })
-  const router = useRouter()
-
   return (
     <>
-      <CardGridTemplate
-        postDatas={data?.pages.flat() ?? []}
-        ref={observerElem}
-        isShowOptions={false}
-      />
-      <CirclePlusButton onClick={() => router.push(APP_PATH.postNew())} />
+      <HydrateCardGrid />
+      <Link href={APP_PATH.postNew()}>
+        <CirclePlusButton />
+      </Link>
     </>
   )
 }
--- src/utils/htmlTagParser.ts
+++ src/utils/htmlTagParser.ts
@@ -1,19 +1,23 @@
-type HTMLTagParserType = (_htmlString: string) => string | null
+type HTMLTagParserType = (_htmlString: string) => string
 
 const elementsToCheck = ['p', 'h6', 'h5', 'h4', 'h3', 'h2', 'h1']
 
 const htmlTagParser: HTMLTagParserType = (htmlString) => {
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(htmlString, 'text/html')
+  try {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(htmlString, 'text/html')
 
-  for (const elementName of elementsToCheck) {
-    const element = doc.querySelector(elementName)
-    if (element) {
-      return element.textContent
+    for (const elementName of elementsToCheck) {
+      const element = doc.querySelector(elementName)
+      if (element) {
+        return element.textContent ?? ''
+      }
     }
-  }
 
-  return null
+    return ''
+  } catch (e) {
+    return ''
+  }
 }
 
 export default htmlTagParser
