8dc07eeb592847191146ba594dd3383b65345d38
이민희 <mhl98@dgu.ac.kr>
2023-09-24 17:47:53 +0900

    :bug: 다크모드 스토리북 인수 전달

--- src/components/atoms/DarkModeButton/index.stories.ts
+++ src/components/atoms/DarkModeButton/index.stories.ts
@@ -9,4 +9,13 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    darkMode: true,
+  },
+  argTypes: {
+    darkMode: {
+      type: 'boolean',
+    },
+  },
+}
