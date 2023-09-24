e20c7fb260381726d1acf78ab151fc678022ff8f
oaoong <pizzachicken@kakao.com>
2023-09-25 01:32:17 +0900

    :sparkles: header 로딩 방식 수정

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,16 +1,39 @@
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 import DarkModeButton from '@/components/atoms/DarkModeButton'
 import NotificationButton from '@/components/atoms/NotificationButton'
 import SearchBar from '@/components/atoms/SearchBar'
 import { Text } from '@/components/atoms/Text'
 import AvatarDropdown from '@/components/molcules/AvatarDropdown/AvatarDropdown'
+import { constants } from '@/config/constants'
+import { Environment } from '@/config/environments'
 import APP_PATH from '@/config/paths'
 import { useDarkmodeCookie } from '@/hooks/useDarkmodeCookie'
-import { validateToken } from '@/services/auth'
+import User from '@/types/user'
 import './index.scss'
 
+const getCurrentUser = async (): Promise<User | null | undefined> => {
+  const cookieStore = cookies()
+  const token = cookieStore.get(constants.AUTH_TOKEN)
+
+  try {
+    const res = await fetch(`${Environment.baseUrl()}/auth-user`, {
+      headers: {
+        Authorization: `Bearer ${token?.value}`,
+      },
+      next: {
+        tags: ['auth'],
+      },
+    })
+    const data = await res.json()
+    return data
+  } catch (e) {
+    return null
+  }
+}
+
 export default async function Header() {
-  const data = validateToken()
+  const currentUser = await getCurrentUser()
 
   let systemDarkmode = false
   if (typeof window !== 'undefined') {
@@ -33,13 +56,13 @@ export default async function Header() {
         <NotificationButton />
         <DarkModeButton darkMode={darkMode} />
       </div>
-      {!data ? (
+      {!currentUser ? (
         <div className="sign-container">
           <LinkButton href={APP_PATH.login()}>로그인</LinkButton>
           <LinkButton href={APP_PATH.register()}>회원가입</LinkButton>
         </div>
       ) : (
-        <AvatarDropdown darkmode={darkMode} />
+        <AvatarDropdown darkmode={darkMode} currentUser={currentUser} />
       )}
     </div>
   )
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,7 +1,5 @@
 import { AxiosError } from 'axios'
-import { useRouter } from 'next/navigation'
 import { notify } from '@/components/atoms/Toast'
-import APP_PATH from '@/config/paths'
 import { SIGN_CONSTANT } from '@/constants/sign'
 import { loginUser } from '@/services/auth'
 
@@ -11,12 +9,11 @@ export interface LoginReqBody {
 }
 
 export const useLogin = () => {
-  const router = useRouter()
   const login = async ({ email, password }: LoginReqBody) => {
     try {
       const res = await loginUser({ email, password })
       if (res) {
-        router.push(APP_PATH.home())
+        location.reload()
       }
     } catch (error) {
       const { response } = error as unknown as AxiosError
