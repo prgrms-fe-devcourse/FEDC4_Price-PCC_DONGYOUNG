6939db4df0355a05fdecebdf2d9c7a976cac1109
Kim0426 <706shin1728@naver.com>
2023-09-24 13:46:52 +0900

    🎉 유저 정보 변경 후 홈으로 이동시 변경 내용 반영되게 설정

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -5,11 +5,13 @@ import { useRouter } from 'next/navigation'
 import CirclePlusButton from '@/components/atoms/CirclePlusButton'
 import CardGridTemplate from '@/components/templates/CardGridTemplate'
 import APP_PATH from '@/config/paths'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
 import useGetAllPosts from '@/queries/channel'
 
 export default function Home() {
-  const { data, fetchNextPage, hasNextPage } = useGetAllPosts()
+  const { currentUser } = useCurrentUser()
+  const { data, fetchNextPage, hasNextPage } = useGetAllPosts(currentUser)
 
   const { observerElem } = useInfiniteScroll({ fetchNextPage, hasNextPage })
   const router = useRouter()
