492812d3bb58ee9170307929fe72e54ed1f912e9
Kim0426 <706shin1728@naver.com>
2023-09-24 23:15:07 +0900

    🐛 getAllUser query Key 수정

--- src/queries/users/index.ts
+++ src/queries/users/index.ts
@@ -1,15 +1,15 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
-import { useAuth } from '@/lib/contexts/authProvider'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { getAllUsers } from '@/services/user'
 import type User from '@/types/user'
 
 const useGetAllUsers = () => {
-  const { currentUser } = useAuth()
+  const { currentUser } = useCurrentUser()
 
   return useQuery({
-    queryKey: ['getAllUsers', currentUser],
+    queryKey: ['getAllUsers', currentUser?._id],
     queryFn: async () => {
       const data: User<string>[] = await getAllUsers()
       return data
