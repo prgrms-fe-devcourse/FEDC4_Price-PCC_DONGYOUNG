82a8fb412592a073cd3a4a9a30b80bdae55322d2
Kim0426 <706shin1728@naver.com>
2023-09-24 22:40:36 +0900

    🧹충돌 오류 해결

--- src/components/atoms/DarkModeButton/index.tsx
+++ src/components/atoms/DarkModeButton/index.tsx
@@ -3,38 +3,11 @@
 import { useState } from 'react'
 import Cookies from 'js-cookie'
 import Assets from '@/config/assets'
-
-import useStorage from '@/hooks/useStorage'
-import useDarkStore from '@/stores/darkMode'
 import ImageButton from '../ImageButton'
 
 type PropsType = {
   darkMode: boolean
 }
-
-
-export default function DarkModeButton({ changeDarkMode }: PropsType) {
-  const [darkMode, setDarkMode] = useStorage<boolean>({
-    storageType: 'local',
-    key: 'pcc-darkmode',
-    initialValue: window.matchMedia('(prefers-color-scheme: dark)').matches,
-  })
-
-  const { toggleState } = useDarkStore()
-
-  useEffect(() => {
-    if (darkMode) {
-      document.body.classList.add('pcc-theme--dark')
-      document.body.classList.remove('pcc-theme--light')
-    }
-  }, [darkMode])
-
-  const handleDarkmodeClick = () => {
-    document.body.classList.toggle('pcc-theme--light')
-    document.body.classList.toggle('pcc-theme--dark')
-    setDarkMode(!darkMode)
-    changeDarkMode(!darkMode)
-    toggleState()
 
 export default function DarkModeButton({ darkMode }: PropsType) {
   const [isDark, setIsDark] = useState(darkMode)
@@ -43,7 +16,6 @@ export default function DarkModeButton({ darkMode }: PropsType) {
     document.body.classList.toggle('pcc-theme--dark')
     Cookies.set('pcc-darkmode', JSON.stringify(!isDark))
     setIsDark(!isDark)
-
   }
   return (
     <ImageButton
