70dc701457f3accd5c9c6c60bb2b6259bbf36ca3
이민희 <mhl98@dgu.ac.kr>
2023-09-25 17:48:59 +0900

    :bug: 모달 켜졌을 때 뒷배경 스크롤 방지

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,7 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function useModal() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'visible'
+    }
+  }, [isModalOpen])
 
   const handleModalOpen = () => {
     setIsModalOpen(true)
