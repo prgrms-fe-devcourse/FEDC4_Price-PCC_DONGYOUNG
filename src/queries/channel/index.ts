f8a1ea8c22725077e4b4af6b40fb42728435bd33
Kim0426 <706shin1728@naver.com>
2023-09-27 17:02:56 +0900

    🎉 불필요한 queryKey부분 제거

--- src/queries/channel/index.ts
+++ src/queries/channel/index.ts
@@ -1,17 +1,10 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { constants } from '@/config/constants'
-import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { getAllPosts } from '@/services/channel'
 
 const useGetAllPosts = () => {
-  const { currentUser } = useCurrentUser()
   return useInfiniteQuery({
-    queryKey: [
-      'getAllPostsInfiniteQuery',
-      currentUser?.fullName,
-      currentUser?.image,
-      currentUser?.notifications,
-    ],
+    queryKey: ['getAllPostsInfiniteQuery'],
     queryFn: ({ pageParam = 0 }) =>
       getAllPosts({
         offset: pageParam,
