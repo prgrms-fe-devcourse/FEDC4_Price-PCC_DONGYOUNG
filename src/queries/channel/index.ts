69043909ba2f1d1cfb3e1063cb35c93f21b78a02
Kim0426 <706shin1728@naver.com>
2023-09-27 09:32:50 +0900

    ✨  querykey의 의존성 추가

--- src/queries/channel/index.ts
+++ src/queries/channel/index.ts
@@ -1,10 +1,17 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { constants } from '@/config/constants'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { getAllPosts } from '@/services/channel'
 
 const useGetAllPosts = () => {
+  const { currentUser } = useCurrentUser()
   return useInfiniteQuery({
-    queryKey: ['getAllPostsInfiniteQuery'],
+    queryKey: [
+      'getAllPostsInfiniteQuery',
+      currentUser?.fullName,
+      currentUser?.image,
+      currentUser?.notifications,
+    ],
     queryFn: ({ pageParam = 0 }) =>
       getAllPosts({
         offset: pageParam,
--- src/queries/users/index.ts
+++ src/queries/users/index.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { getAllUsers } from '@/services/user'
 import { UserSummary } from '@/types/user'
 
 const useGetAllUsers = () => {
+  const { currentUser } = useCurrentUser()
   return useQuery({
-    queryKey: ['getAllUsers'],
+    queryKey: [
+      'getAllUsers',
+      currentUser?._id,
+      currentUser?.coverImage,
+      currentUser?.fullName,
+      currentUser?.name,
+    ],
     queryFn: async () => {
       const data: UserSummary[] = await getAllUsers()
       return data
